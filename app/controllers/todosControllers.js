0a1e8b38c0830b15c79cb20fe3e2f03dcbc5e70c
Momin Riyadh <mominriyadh@gmail.com>
2023-11-27 15:39:42 +0600

    fix and deploy put routes

--- app/controllers/todosControllers.js
+++ app/controllers/todosControllers.js
@@ -1,18 +1,52 @@
 const asyncHandler = require('express-async-handler');
+const Todo = require('../models/todoModel');
 
-const getTodos = (req, res) => {
-    res.status(200).json({message: 'Get todos'})
-}
-const setTodo = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Get todos'})
+// GET Todos List
+const getTodos = asyncHandler(async (req, res) => {
+    const todos = await Todo.find({});
+    res.status(200).json(todos);
 });
 
-const updateTodo = (req, res) => {
-    res.status(200).json({message: `Update todos ${req.params.id}`})
-}
-const deleteTodo = (req, res) => {
-    res.status(200).json({message: `Delete todos ${req.params.id}`})
-}
+
+//POST Todos Items
+const setTodo = asyncHandler(async (req, res) => {
+    if (!req.body.text) {
+        res.status(400).json({message: 'Please add text field'});
+        return;
+    }
+    const todo = await Todo.create({
+        text: req.body.text,
+    });
+
+    res.status(200).json(todo);
+});
+
+
+//Update Todos items
+const updateTodo = asyncHandler(async (req, res) => {
+    const todo = await Todo.findById(req.params.id);
+
+    if (!todo) {
+        res.status(400);
+        throw new Error('Todo not found');
+    }
+
+    const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+    });
+    res.status(200).json(updatedTodo);
+})
+
+
+//DELETE Todos Items
+const deleteTodo = asyncHandler(async (req, res) => {
+    const todo = await Todo.findById(req.params.id);
+
+    if (!todo) {
+        res.status(400);
+        throw new Error('Todo not found');
+    }
+})
 
 module.exports = {
     getTodos, setTodo, updateTodo, deleteTodo
