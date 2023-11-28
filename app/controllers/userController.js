75eac3e29ee3cf02e12b505bc25e879dbb867b6b
Momin Riyadh <mominriyadh@gmail.com>
2023-11-28 11:48:14 +0600

    fix and deploy user routes

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,13 +1,70 @@
-const registerUser = (req, res) => {
-    res.json({ message: "Register User" });
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
+
+// Generate JWT
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: "30d"});
+}
+
+//Create/Register User
+const registerUser = asyncHandler(async (req, res) => {
+    const {name, email, password} = req.body;
+
+    if (!name || !email || !password) {
+        res.status(400);
+        throw new Error("Please add all fields");
+    }
+    // check if user exists
+    const userExists = await User.findOne({email});
+    if (userExists) {
+        res.status(400);
+        throw new Error("User already exists");
+    }
+    // create hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    //create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
+        })
+    } else {
+        res.status(400);
+        throw new Error("Invalid user data")
+    }
+
+});
+
+
+// Login/Authenticate User
+const loginUser = (req, res) => {
+    const {email, password} = req.body;
+
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("Please add all fields");
+    }
+
+    //Check for user Email
+    const user = await User.findOne({email});
 };
 
-const loginUser = (req, res) => {
-    res.json({ message: "Login User" });
-};
+
 
 const getLoggedInUser = (req, res) => {
-    res.json({ message: "Logged In User data" });
+    res.status(200).json({message: "Logged In User data"});
 };
 
 module.exports = {
