d39542c247b81e800c73cea9e754997e2304c9f7
Momin Riyadh <mominriyadh@gmail.com>
2023-11-28 10:53:43 +0600

    fix and deploy user routes

--- app/config/db.js
+++ app/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`Mongo db connected: ${conn.connection.host}`);
+        console.log(`Mongo db connected and port running on ${process.env.PORT}`);
     } catch (error) {
         console.log(error);
         process.exit(1);
